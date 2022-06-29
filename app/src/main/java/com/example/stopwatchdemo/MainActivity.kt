f68d82517ce051e8f2aa9378f4733b52f7d4948c
Danish Khan <iamdanishk555@gmail.com>
2022-06-29 21:19:25 +0545

    viewbinding added and UI setup

--- app/src/main/java/com/example/stopwatchdemo/MainActivity.kt
+++ app/src/main/java/com/example/stopwatchdemo/MainActivity.kt
@@ -2,8 +2,13 @@ package com.example.stopwatchdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.stopwatchdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var isStarted = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
